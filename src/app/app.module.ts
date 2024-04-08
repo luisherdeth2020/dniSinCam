50db831fe7b8baa428b9f12f7c8bac2d52aec1f3
Luis Herdeth <luis.herdeth@gmail.com>
2024-04-08 07:23:32 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { AppComponent } from './app.component';
+import { ScannerComponent } from './scanner/scanner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScannerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ZXingScannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
